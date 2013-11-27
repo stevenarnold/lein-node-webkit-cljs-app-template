603105139a0107200e448cd04bf17fb49f8987fd
Frank Hale <frankhale@gmail.com>
2013-11-27 15:32:37 -0500

    Change URL in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject node-webkit-cljs-app/lein-template "0.1.0"
   :description "Lein template for a Node-Webkit Clojurescript app."
-  :url "http://github.com/frankhale/node-webkit-cljs-app"
+  :url "https://github.com/frankhale/lein-node-webkit-cljs-app-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
