7fccc169a0650d3bb54f39f7ebdff128b2139707
Frank Hale <frankhale@gmail.com>
2014-04-06 22:32:15 -0400

    Update Clojurescript and cljsbuild
    Something isn't quite right about Clojure 1.6.0 and jayq. Got an odd
    error that was screwing things up. Will need to investigate further!

--- src/leiningen/new/node_webkit_cljs_app/app/project.clj
+++ src/leiningen/new/node_webkit_cljs_app/app/project.clj
@@ -3,13 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]]
-  :plugins [[lein-cljsbuild "1.0.2"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2202"]]
+  :plugins [[lein-cljsbuild "1.0.3"]
 			[org.bodil/lein-noderepl "0.1.11"]]
   :cljsbuild {
     :builds {
              :dev {:source-paths ["src"]
-                   :compiler {:output-to "resources/scripts/main.js"
-							  :optimizations :whitespace
-                              :pretty-print true}}}})
+									 :compiler {:output-to "resources/scripts/main.js"
+									 :optimizations :whitespace
+									 :pretty-print true}}}})
